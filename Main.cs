1f3c4aedadb262ae6f4771ea9282f29c26e6cf39
Arix581 <113464283+Arix581@users.noreply.github.com>
2024-02-25 17:17:17 -0700

    Update Main.cs V1.1

--- Main.cs
+++ Main.cs
@@ -1,5 +1,6 @@
 // Online C# Editor for free
 // Write, Edit and Run your C# code using C# Online Compiler
+// V1.1
 
 using System;
 
@@ -11,3 +12,39 @@ public class HelloWorld
     }
 }
 
+public class Deck
+{
+    public Suit[] possibleSuits;
+    public int[] possibleNumbers;
+    
+    public Deck(Suit[] suits, int[] numbers)
+    {
+        for (int s = 0; s < suits.Length; s++)
+        {
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                Console.WriteLine(s + " " + n);
+            }
+        }
+    }
+}
+
+public class Card
+{
+    public Suit suit;
+    public int number;
+    
+    public Card(Suit suit2, int number2)
+    {
+        suit = suit2;
+        number = number2;
+    }
+}
+
+public enum Suit 
+{
+    Spades = 4,
+    Hearts = 3,
+    Clubs = 2,
+    Diamonds = 1
+}
