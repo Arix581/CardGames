65752c6e6c8d1a0db10f2b9561ba0f9235cdbfa5
Arix581 <113464283+Arix581@users.noreply.github.com>
2024-03-07 21:13:20 -0700

    Update Main.cs V1.4

--- Main.cs
+++ Main.cs
@@ -1,6 +1,6 @@
 // Online C# Editor for free
 // Write, Edit and Run your C# code using C# Online Compiler
-// V1.3
+// V1.4
 
 using System;
 using System.Collections.Generic;
@@ -10,14 +10,51 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         Console.WriteLine ("Hello Mono World");
+        
+        Card card1 = new Card(Suit.Spades, 1);
+        Card card2 = new Card(Suit.Hearts, 2);
+        Card card3 = new Card(Suit.Clubs, 3);
+        
+        Player person = new Player(99, "Test Player 1");
+        //person.hand.Add(new Card(Suit.Spades, 1));
+        //player.hand.Add(card2);
+        //player.hand.Add(card3);
+        //Console.WriteLine(player);
+        
     }
 }
 
-static public class NinetyNineGame
+public class NinetyNineGame
 {
     public Player[] players;
     public Deck deck;
-    // public Score[] cardValues
+    public Score[] cardValues = {
+        new Score(1, 1),
+        new Score(2, 2),
+        new Score(3, 0),
+        new Score(4, 0),
+        new Score(5, 5),
+        new Score(6, 6),
+        new Score(7, 7),
+        new Score(8, 8),
+        new Score(9, 0),
+        new Score(10, -10),
+        new Score(11, 10),
+        new Score(12, 10),
+        new Score(13, 10)
+    };
+}
+
+public struct Score 
+{
+    public int number;
+    public int modifier;
+    
+    public Score(int number_, int modifier_)
+    {
+        number = number_;
+        modifier = modifier_;
+    }
 }
 
 public class Player 
@@ -27,9 +64,10 @@ public class Player
     public string name;
     public int tokens;
     
-    public Player(int handSize)
+    public Player(int handSize, string name_)
     {
         maxHandSize = handSize;
+        name = name_;
     }
     
     public void Discard(Deck deck, Card toDiscard)
@@ -45,6 +83,27 @@ public class Player
             Card drawn = deck.Draw();
             hand.Add(drawn);
         }
+    }
+    
+    public override string ToString() // Returns string as a hand
+    {
+        int last = hand.Count - 1;
+        string toReturn = name + " has";
+        for (int i = 0; i < hand.Count; i++)
+        {
+            if (i == last)
+            {
+                toReturn += " and";
+            }
+            toReturn += " a " + hand[i];
+            if (i < last)
+            {
+                toReturn += ",";
+            }
+        }
+        toReturn += ".";
+        return toReturn;
+        
     }
 }
 
@@ -91,6 +150,11 @@ public class Card
     {
         suit = suit2;
         number = number2;
+    }
+    
+    public override string ToString()
+    {
+        return suit + " of " + number;
     }
 }
 
